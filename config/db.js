901a5b726dd4be410397893652b9ef3fe55f6338
Mohammad Ali <mali@betahubs.com>
2017-03-04 16:20:27 +0200

    adding lenode db creds

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//     host     : 'us-cdbr-iron-east-04.cleardb.net',
+//     user     : 'b657486ddf60b2',
+//     password : '33911054',
+//     database : 'ad_f47af548feb67bb'
+// });
 var connection = mysql.createConnection({
-    host     : 'us-cdbr-iron-east-04.cleardb.net',
-    user     : 'b657486ddf60b2',
-    password : '33911054',
-    database : 'ad_f47af548feb67bb'
+    host     : 'localhost',
+    user     : 'wunderlust_db',
+    password : 'Hja99_2a..1@',
+    database : 'wunderlust'
 });
 
 connection.connect(function(err) {
